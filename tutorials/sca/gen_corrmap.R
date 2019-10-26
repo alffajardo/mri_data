b1ec0aee4104b59ec26c39ea5c967ca97a22cffc
Alfonso Fajardo <faj.alf@gmail.com>
2019-10-26 15:01:39 -0500

    Update gen_corrmap.R

--- tutorials/sca/gen_corrmap.R
+++ tutorials/sca/gen_corrmap.R
@@ -8,23 +8,27 @@ output <- as.character(args[3])
 library(magrittr)
 library(oro.nifti)
 library(neurobase)
+
+## read data
 func <- RNifti::readNifti(dataset)
 roi <- readNIfTI(roi,reorient = FALSE)
 d <- dim(func)
 
+### creat mask
 func_mask <- niftiarr(img= roi, arr = 0)
 func_mask@.Data <- func[,,,1]
 func_mask[func_mask !=0] <- 1
 
-###
+### time series
 ts <- matrix(func[roi!=0],ncol = d[4]) %>%
       colMeans()
-
+## linearize data and compute correlation matrix
 rvalues <- matrix(func[func_mask !=0],ncol=d[4]) %>%
           apply(.,1,cor,ts)
           
-
+### reshape computed data to create whole brain map
 rmap <- func_mask
 rmap[rmap !=0] <- rvalues
 
+### write output in nifti format
 write_nifti(rmap,output)
