67d506aaacadc0de0029e0572f8dc949f0cae4e5
Alfonso Fajardo <faj.alf@gmail.com>
2019-10-28 16:27:38 -0600

    Update gen_corrmap.R

--- tutorials/sca/gen_corrmap.R
+++ tutorials/sca/gen_corrmap.R
@@ -30,7 +30,7 @@ func_mask[func_mask !=0] <- 1
 ### time series
 ts <- matrix(func[roi!=0],ncol = d[4]) %>%
       colMeans()
-## linearize non zero voxelsand compute correlation coeficients in every row
+## linearize non zero voxels and compute correlation coeficients in every row
 rvalues <- matrix(func[func_mask !=0],ncol=d[4]) %>%
           apply(.,1,cor,ts)
           
